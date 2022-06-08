c63f03747769123418a270e1d2e1a27805a57579
david-lafontant <lafontant.david@gmail.com>
2022-06-08 06:13:03 -0600

    Generate users controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users , only: %i[index show]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
