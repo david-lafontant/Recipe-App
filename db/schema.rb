5c65ba20346e7bf76c40d43bc464d638e70716de
david-lafontant <lafontant.david@gmail.com>
2022-06-09 09:00:30 -0600

    Add recipe food model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_145112) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_145744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_145112) do
   end
 
   create_table "inventory_foods", force: :cascade do |t|
+    t.float "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
     t.float "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
