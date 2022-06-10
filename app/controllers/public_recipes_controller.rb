50626f574fec483543d696515d3196f8f932a6b6
david-lafontant <lafontant.david@gmail.com>
2022-06-10 10:48:50 -0600

    Update public recipe controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,6 @@
-class PublicRecipesController < ApplicationController
+class PublicRecipesController < ApplicationController 
   def index
+  @public_recipe = Recipe.new
+  @public_recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
