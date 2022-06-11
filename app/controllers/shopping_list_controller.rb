a6239b784476d1f6ea2b66a7bcaa7806539b55e6
david-lafontant <lafontant.david@gmail.com>
2022-06-11 13:28:12 -0600

    Fix linter errors

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -5,8 +5,8 @@ class ShoppingListController < ApplicationController
     @total_value = 0
     @total_items = 0
     @recipe_foods.each do |f|
-    @total_value += f.quantity * f.food.price
-    @total_items += 1
+      @total_value += f.quantity * f.food.price
+      @total_items += 1
     end
   end
 end
