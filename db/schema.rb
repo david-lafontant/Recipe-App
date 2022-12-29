6d1cea86edc3991dd2935d585f12ee192ef9ecde
david-lafontant <lafontant.david@gmail.com>
2022-12-28 23:14:02 -0600

    Remove confirmable function in development mode

--- db/schema.rb
+++ db/schema.rb
@@ -79,9 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_153330) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
