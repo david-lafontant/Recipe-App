a07a06b7b1892862883b8c205bbe67cd4e4d36ec
David Lafontant <lafontant.david@gmail.com>
2023-08-24 17:18:26 -0400

    Add config setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,12 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.config.hosts << "recipe-app-21hg.onrender.com"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << "recipe-app-214z.onrender.com"
+  config.hosts << "recipe-app-21hg.onrender.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
